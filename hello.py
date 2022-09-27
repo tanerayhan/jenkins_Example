992bb6e16e1f205ee556380316065ad9ad6342bd
tanerayhan <54556855+tanerayhan@users.noreply.github.com>
2022-09-27 12:01:57 +0300

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,25 +1,22 @@
 from rich.table import Table as RichTable
-from rich import print as rich_print
+from rich.console import Console as RichConsole
 import numpy as np
 
 
 class RichTableCreator:
     """RichTable Class"""
 
-    def __init__(self, product):
+    def __init__(self):
         self.index = 0
-        self.product = product
-
-        self.table = RichTable(
-            title=self.product.upper(), show_lines=False, expand=True
-        )
-        self.table.add_column("Chain", style="cyan")
-        self.table.add_column("AB", style="magenta")
-        self.table.add_column("AC", style="magenta")
-        self.table.add_column("AD", style="magenta")
-        self.table.add_column("BC", style="magenta")
-        self.table.add_column("BD", style="magenta")
-        self.table.add_column("CD", style="magenta")
+        self.verification_data = []
+        self.table = RichTable(title="Verification", show_lines=True)
+        self.console = RichConsole()
+        self.table.add_column("Index", style="cyan")
+        self.table.add_column("Step", style="magenta", min_width=50)
+        self.table.add_column("Envelope | Tested", style="magenta")
+        self.table.add_column("Config | Expected", style="magenta")
+        self.table.add_column("AirSniff", style="magenta")
+        self.table.add_column("Result", style="green")
 
     def update(self, _list):
         """table update"""
@@ -31,50 +28,45 @@ class RichTableCreator:
             str(_list[2]),
             str(_list[3]),
             str(_list[4]),
-            str(_list[5]),
         )
 
     def print(self, _list):
         """show the table"""
         try:
-            num_of_path = 6
+            num_of_path = 5
             num_of_chain = int(len(_list) / num_of_path)
             _list = np.reshape(_list, (num_of_chain, num_of_path))
             for _i_ in range(num_of_chain):
                 temp_list = _list[_i_, :]
                 self.update(temp_list)
 
-            rich_print(self.table)
+            self.console.print(self.table)
         except Exception as err:
             return False
         return True
 
 
-rich_table = RichTableCreator(product="Taner")
+rich_table = RichTableCreator()
 mylist = [
     "AASSADFASDGKASKDGKASGKAKSDGKSKDGA",
     2,
     3,
     4,
-    "AASSADFASDG",
+    5,
     6,
     "AASSADFASDGKASKDGKASGKAKSDGKSKDGA",
     8,
     9,
     10,
-    "AASSADFASDG",
+    11,
     12,
     "AASSADFASDGKASKDGKASGKAKSDGKSKDGA",
     14,
     15,
     16,
-    "AASSADFASDG",
+    17,
     18,
     "AASSADFASDGKASKDGKASGKAKSDGKSKDGA",
     20,
-    21,
-    22,
-    "AASSADFASDG",
-    24,
 ]
 rich_table.print(mylist)
