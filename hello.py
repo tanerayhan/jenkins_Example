fb174464969a64370ec48d3f8100300f177bf510
tanerayhan <54556855+tanerayhan@users.noreply.github.com>
2022-09-29 18:39:21 +0300

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,21 +1,79 @@
+from rich.table import Table as RichTable
+from rich.console import Console as RichConsole
+from rich.console import ConsoleOptions
 from rich import print as rich_print
-from rich.table import Table
+import numpy as np
+from tabulate import tabulate
 
 
-table = Table(title="Star Wars Movies", width=150)
+class RichTableCreator:
+    """RichTable Class"""
 
-table.add_column("Released", justify="right", style="cyan", no_wrap=True)
-table.add_column("Title", style="magenta")
-table.add_column("Box Office", justify="right", style="green")
+    def __init__(self):
+        self.index = 0
+        self.verification_data = []
+        self.table = RichTable(
+            title="Verification",
+            show_lines=True,
+        )
+        self.console = RichConsole(width=250)
+        self.table.add_column("Index")
+        self.table.add_column("Step")
+        self.table.add_column("Envelope | Tested")
+        self.table.add_column("Config | Expected")
+        self.table.add_column("AirSniff")
+        self.table.add_column("Result")
 
-table.add_row(
-    "Dec 20, 2019",
-    "Star Wars: TheAKSDGKASDKGKASDGKASKDGKASGDKGDSAKKAGSDKGSD Rise of Skywalker",
-    "$952,110,690",
-)
-table.add_row("May 25, 2018", "Solo: A Star Wars Story", "$393,151,347")
-table.add_row("Dec 15, 2017", "Star Wars Ep. V111: The Last Jedi", "$1,332,539,889")
-table.add_row("Dec 16, 2016", "Rogue One: A Star Wars Story", "$1,332,439,889")
+    def update(self, _list):
+        """table update"""
+        self.index += 1
+        self.table.add_row(
+            str(self.index),
+            str(_list[0]),
+            str(_list[1]),
+            str(_list[2]),
+            str(_list[3]),
+            str(_list[4]),
+        )
+
+    def print(self, _list):
+        """show the table"""
+        try:
+            num_of_path = 5
+            num_of_chain = int(len(_list) / num_of_path)
+            _list = np.reshape(_list, (num_of_chain, num_of_path))
+            for _i_ in range(num_of_chain):
+                temp_list = _list[_i_, :]
+                self.update(temp_list)
+            self.console.print(self.table)
+            rich_print(self.table)
+
+        except Exception as err:
+            return False
+        return True
 
 
-rich_print(table)
+rich_table = RichTableCreator()
+mylist = [
+    "AASSADFASDGKAS GKAKSDG",
+    2,
+    3,
+    4,
+    5,
+    "AirtiesWirelessNetwork",
+    "AASSADFASGKA GKSKDGA",
+    8,
+    9,
+    10,
+    11,
+    12,
+    "AASSADFASDGKAS GKADGA",
+    14,
+    15,
+    16,
+    17,
+    18,
+    "AASSADFASDGKASKDGKAAA",
+    20,
+]
+rich_table.print(mylist)
